c8972fecf016ecd409291ff8b94fc9c4c14b3fee
Narammanoj <Narammanoj99@gmail.com>
2024-09-19 20:19:20 -0400

    first files

--- week2classlab/App.tsx
+++ week2classlab/App.tsx
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , ActivityIndicator, Image} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Text 1</Text>
+      <Text>Text 2</Text>
+      <ActivityIndicator />
+      <Image 
+        source={{uri:'https://reactnative.dev/img/tiny_logo.png'}} 
+        style={styles.tinyLogo} >
+        </Image>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +23,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  tinyLogo: {
+    width: 500,
+    height: 500,
   },
 });
